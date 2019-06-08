c8eed30ded5d75fe63ce81ffecb78f07f52c2c03
Tlili <tpdrn.france@gmail.com>
2019-06-08 22:56:27 +0200

    App config step 1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, CacheInterceptor, UseInterceptors, HttpException, HttpStatus, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
+@UseInterceptors(CacheInterceptor)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly _appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({
+    description: 'Get all employees',
+    title: 'Get all',
+    operationId: 'GET /employees'
+  })
+  @ApiResponse({ status: 200, description: 'Get all employees' })
+  @ApiResponse({ status: 403, description: "Unauthorized" })
+  @ApiResponse({ status: 404, description: "Not found" })
+  @ApiResponse({ status: 500, description: "Internal server error" })
+  getOne(): string {
+    Logger.log("Get One", "AppController");
+    return this._appService.getHello();
   }
 }
