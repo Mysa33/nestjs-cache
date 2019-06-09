4b6704971b1aaf3fc30f8606c0fa6b7b37dc60dc
Tlili <tpdrn.france@gmail.com>
2019-06-09 15:39:49 +0200

    App config step 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,28 @@
-import { Controller, Get, CacheInterceptor, UseInterceptors, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { Controller, Get, CacheInterceptor, UseInterceptors, Logger, Param, HttpStatus, HttpException } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiModelProperty, ApiImplicitParam } from '@nestjs/swagger';
 
-@Controller()
+@Controller('items')
 @UseInterceptors(CacheInterceptor)
 export class AppController {
+
   constructor(private readonly _appService: AppService) {}
 
-  @Get()
-  @ApiOperation({
-    description: 'Get all employees',
-    title: 'Get all',
-    operationId: 'GET /employees'
-  })
-  @ApiResponse({ status: 200, description: 'Get all employees' })
+  @Get(':itemId')
+  @ApiOperation({description: 'Get one item',title: 'Get item',operationId: 'GET /items'})
+  @ApiImplicitParam({name:'Item ID',description:'Item id',required:true})
+  @ApiModelProperty({description:'Return an object', required:true})
+  @ApiResponse({ status: 200, description: 'Get one item' })
   @ApiResponse({ status: 403, description: "Unauthorized" })
   @ApiResponse({ status: 404, description: "Not found" })
   @ApiResponse({ status: 500, description: "Internal server error" })
-  getOne(): string {
+  async getOne(itemId:number): Promise<object[]> {
     Logger.log("Get One", "AppController");
-    return this._appService.getHello();
+    const item = await this._appService.getOneItem(itemId);
+    if(item){
+      return item;
+    }else{
+      throw new HttpException('Employee not found', HttpStatus.NOT_FOUND);
+    } 
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly _id:number
+  getOneItem(_id): any[] {
+    return [];
   }
 }
